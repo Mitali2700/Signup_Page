19a0550116d6b4fe92b4244a3b57bf292a72e6fc
Mitali2700 <201B155@juetguna.in>
2022-08-06 15:26:31 +0530

    adding app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 //jshint esversion: 6
 
 const express = require("express");
@@ -9,5 +10,3 @@ const app = express();
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
-
-
