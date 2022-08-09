98f49b62cc83e8384155d209603a6d64375aa713
Mitali2700 <201B155@juetguna.in>
2022-08-09 22:12:38 +0530

    setting up server

--- app.js
+++ app.js
@@ -7,9 +7,26 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/",function(_req,res){
+app.get("/",function(req,res){
     res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+    // console.log(firstName);
+    // console.log("\n");
+    // console.log(lastName);
+    // console.log("\n");
+    // console.log(email);
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);  
+
 });
 
 app.listen(3000, function(){
