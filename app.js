f8432d0941c8aab3e0b61251650b41d8f6345859
Mitali2700 <201B155@juetguna.in>
2022-08-11 17:36:13 +0530

    setting mailchimp's Server

--- app.js
+++ app.js
@@ -28,7 +28,34 @@ app.post("/",function(req,res){
     console.log(email);  
 
 });
+// we're going to set our web page up with the MailChimp API to start sending this data over to their servers. 
+
+
+/*
+After creating account :
+Go to your name then click on "Account"  ->  "Extras"  ->  "API Key"
+After clicking on "API key" -> Click on "Create a key"
+Now go to "mailchimp.com/developer/"
+
+Now go to:
+https://mailchimp.com/developer/marketing/api/batch-operations/
+
+Now go to: List/Audiences
+https://mailchimp.com/developer/marketing/api/lists/
+
+Now do this :
+https://mailchimp.com/developer/marketing/api/lists/batch-subscribe-or-unsubscribe/
+
+Now go to mailchimp.com  -->  Audience
+
+*/
+
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
+
+// 687eca5f53384623bcc65d49e7590f57-us8
+// dc0ebebe7317ac4ca1347fd5c4652150-us8
+
